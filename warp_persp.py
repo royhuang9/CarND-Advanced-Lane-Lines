fa1487449c95071f1f788da45311e92abe84a108
Roy Huang <royhuang9@hotmail.com>
2017-02-23 12:43:57 +0800

    update warp_persp

--- warp_persp.py
+++ warp_persp.py
@@ -10,14 +10,10 @@ import numpy as np
 import pickle
 import cv2
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
-
-#src = np.asarray([[527, 500], [763, 500], [910, 600], [392, 600]], dtype=np.float32)
 src = np.asarray([[524, 500], [768, 500], [910, 600], [392, 600]], dtype=np.float32)
-#src = np.asarray([[522, 500], [769, 500], [910, 600], [392, 600]], dtype=np.float32)
-#dst = np.asarray([[392, 300], [910, 300], [910, 600], [390, 600]], dtype=np.float32)
 dst = np.asarray([[196, 400], [455, 400], [455, 600], [196, 600]], dtype=np.float32)
-#dst = np.asarray([[196, 300], [455, 300], [455, 600], [196, 600]], dtype=np.float32)
 
 mt_persp=cv2.getPerspectiveTransform(src, dst)
 
@@ -30,12 +26,12 @@ camera_mtx = data['cal']
 dist_coeff = data['dist']
 new_camera_mtx = data['new']
 
-file_name = './test_images/straight_lines1.jpg'
-#file_name = './test_images/test5.jpg'
+#file_name = './test_images/straight_lines1.jpg'
+file_name = './test_images/test2.jpg'
+
 image = np.asarray(Image.open(file_name).convert('L'))
 img_undist = cv2.undistort(image, camera_mtx, dist_coeff, None, new_camera_mtx)
 
-#img_size = (image.shape[1]//2, image.shape[0])
 img_size = (image.shape[1]//2, image.shape[0])
 
 data = {'mt':mt_persp, 'img_size':img_size}
@@ -47,25 +43,28 @@ img_h, img_w = image.shape
 
 img_warped = cv2.warpPerspective(img_undist, mt_persp, img_size)
 
-plt.figure(figsize=(12,9))
-plt.gray()
-plt.imshow(img_warped)
 
-roi1 = img_warped[:, 150:270]
+fig = plt.figure(figsize=(12,9))
+gs = gridspec.GridSpec(1, 2,
+                       width_ratios=[2,1],
+                       height_ratios=[1,1]
+                       )
 
-img3 = np.dstack((img_warped, img_warped, img_warped));
+ax1 = plt.subplot(gs[0])
+ax2 = plt.subplot(gs[1])
 
-img3[:, (150+257):(270+257), 2] = roi1
+ax1.imshow(img_undist, cmap='Greys_r')
+ax1.plot(src[:,0], src[:,1], 'ro')
+ax1.axis('off')
+ax1.set_title('Straight Line 1, undistorted')
+ax1.set_xlim(0, image.shape[1])
+ax1.set_ylim(image.shape[0], 0)
 
-plt.figure(figsize=(12,9))
-plt.gray()
-plt.imshow(img3)
+ax2.imshow(img_warped, cmap='Greys_r')
+ax2.plot(dst[:,0], dst[:,1],'ro')
+ax2.axis('off')
+ax2.set_title('Bird view')
+ax2.set_xlim(0, img_size[0])
+ax2.set_ylim(img_size[1], 0)
 
-'''
-image2 = np.asarray(Image.open('./test_images/straight_undist2.jpg').convert('L'))
-
-img2_warped = cv2.warpPerspective(image2, mt_persp, img_size)
-plt.figure(figsize=(12,9))
-plt.gray()
-plt.imshow(img2_warped)
-'''
+fig.tight_layout()
